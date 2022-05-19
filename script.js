175cfbbadbc50ece9631d7f3ef7f2cd51a849a92
qzmp10 <loganrobitaille@outlook.com>
2022-05-19 10:34:30 -0400

    Added some alerts when user tries to interact with mock UI

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let notif = document.querySelector('.bell');
 let popUp = document.querySelector('.bellWindow');
+let sidebarElements = document.querySelectorAll('.sideInfo');
+let errorWindow = document.querySelector('.errorWindow');
+let headerBtns = document.querySelectorAll('.btn');
 
 notif.addEventListener('mouseover', () => {
     popUp.classList.add('popUp');
@@ -10,6 +13,31 @@ notif.addEventListener('mouseout', () => {
     popUp.textContent = "";
     popUp.classList.remove('popUp');
 });
+
+sidebarElements.forEach(sidebarElement => {
+    sidebarElement.addEventListener('click', () => {
+        errorWindow.classList.add('mockUi');
+        errorWindow.textContent = "❌ This is a mock UI! You cannot interact with it!";
+    });
+    sidebarElement.addEventListener('mouseout', () => {
+        errorWindow.classList.remove('mockUi');
+        errorWindow.textContent = "";
+    });
+});
+
+headerBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        errorWindow.classList.add('mockUi');
+        errorWindow.textContent = "❌ This is a mock UI! You cannot interact with it!";
+    });
+    button.addEventListener('mouseout', () => {
+        errorWindow.classList.remove('mockUi');
+        errorWindow.textContent = "";
+    });
+});
+
+
+
 
 
 

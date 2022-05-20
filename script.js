1f9084549f1b224fe5987096c4b92d7bf6eb31e8
qzmp10 <loganrobitaille@outlook.com>
2022-05-20 13:30:18 -0400

    Finished the slideshow of my projects on desktop

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//variables for the popups when hovering/ clicking sidebar elements and/or buttons
 let notif = document.querySelector('.bell');
 let popUp = document.querySelector('.bellWindow');
 let sidebarElements = document.querySelectorAll('.sideInfo');
@@ -8,6 +9,13 @@ let slideShow = document.querySelector('.slideshow');
 let leftArrow = document.querySelector('.left-arrow');
 let rightArrow = document.querySelector('.right-arrow');
 
+//default project for the slideshow
+slideShow.href = "https://qzmp10.github.io/etch-a-sketch/";
+
+//slide text content
+let slideText = document.querySelector('.slide-text > p');
+
+//Warning pops up when user tries to click something
 notif.addEventListener('mouseover', () => {
     popUp.classList.add('popUp');
     popUp.textContent = "✓ No new notifications.";
@@ -40,9 +48,116 @@ headerBtns.forEach(button => {
     });
 });
 
+//Clicking arrow changes link of element, and animates it
 leftArrow.addEventListener('click', () => {
-    slideShow.href = "https://www.theodinproject.com/lessons/foundations-fundamentals-part-3";
-})
+
+    if (slideShow.href == "https://qzmp10.github.io/etch-a-sketch/") {
+        slideShow.classList.add('slide-transition');
+        slideShow.addEventListener('transitionend', () => {
+
+            // when 1st transition ends, go to calculator
+            slideShow.classList.add('slide-transition-2');
+            slideShow.href = "https://qzmp10.github.io/calculator/";
+            slideShow.setAttribute('style', 'background-image: linear-gradient(slategray, white)');
+            slideText.textContent = 'Calculator';
+            slideShow.classList.remove('slide-transition');
+
+            //when 2nd transition ends, remove class
+            slideShow.addEventListener('transitionend', () => {
+                slideShow.classList.remove('slide-transition-2');
+            });
+        });
+
+    } else if (slideShow.href == "https://qzmp10.github.io/calculator/") {
+        slideShow.classList.add('slide-transition');
+        slideShow.addEventListener('transitionend', () => {
+
+           // when 1st transition ends, go to sign-up form, remove 1st slide class
+            slideShow.classList.add('slide-transition-2');
+            slideShow.href = 'https://qzmp10.github.io/signup_form/';
+            slideShow.setAttribute('style', 'background-image: linear-gradient(rgb(0, 0, 0), rgb(29, 245, 0))');
+            slideText.textContent = 'Sign-up form';
+            slideShow.classList.remove('slide-transition');
+            
+            //when 2nd transition ends, remove class
+            slideShow.addEventListener('transitionend', () => {
+                slideShow.classList.remove('slide-transition-2');
+            });
+        });
+
+    } else if (slideShow.href == 'https://qzmp10.github.io/signup_form/') {
+        slideShow.classList.add('slide-transition');
+        slideShow.addEventListener('transitionend', () => {
+
+           // when 1st transition ends, go to sign-up form, remove 1st slide class
+            slideShow.classList.add('slide-transition-2');
+            slideShow.href = "https://qzmp10.github.io/etch-a-sketch/";
+            slideShow.setAttribute('style', 'background-image: linear-gradient(rgb(162, 0, 255), rgb(255, 174, 0))');
+            slideText.textContent = 'Etch-a-sketch';
+            slideShow.classList.remove('slide-transition');
+            
+            //when 2nd transition ends, remove class
+            slideShow.addEventListener('transitionend', () => {
+                slideShow.classList.remove('slide-transition-2');
+            });
+        });
+    }
+});
+
+rightArrow.addEventListener('click', () => {
+
+    if (slideShow.href == "https://qzmp10.github.io/etch-a-sketch/") {
+        slideShow.classList.add('slide-transition');
+        slideShow.addEventListener('transitionend', () => {
+
+            // when 1st transition ends, go to calculator
+            slideShow.classList.add('slide-transition-2');
+            slideShow.href = 'https://qzmp10.github.io/signup_form/';
+            slideShow.setAttribute('style', 'background-image: linear-gradient(rgb(0, 0, 0), rgb(29, 245, 0))');
+            slideText.textContent = 'Sign-up form';
+            slideShow.classList.remove('slide-transition');
+
+            //when 2nd transition ends, remove class
+            slideShow.addEventListener('transitionend', () => {
+                slideShow.classList.remove('slide-transition-2');
+            });
+        });
+
+    } else if (slideShow.href == 'https://qzmp10.github.io/signup_form/') {
+        slideShow.classList.add('slide-transition');
+        slideShow.addEventListener('transitionend', () => {
+
+           // when 1st transition ends, go to sign-up form, remove 1st slide class
+            slideShow.classList.add('slide-transition-2');
+            slideShow.href = "https://qzmp10.github.io/calculator/";
+            slideShow.setAttribute('style', 'background-image: linear-gradient(slategray, white)');
+            slideText.textContent = 'Calculator';
+            slideShow.classList.remove('slide-transition');
+            
+            //when 2nd transition ends, remove class
+            slideShow.addEventListener('transitionend', () => {
+                slideShow.classList.remove('slide-transition-2');
+            });
+        });
+
+    } else if (slideShow.href == "https://qzmp10.github.io/calculator/") {
+        slideShow.classList.add('slide-transition');
+        slideShow.addEventListener('transitionend', () => {
+
+           // when 1st transition ends, go to sign-up form, remove 1st slide class
+            slideShow.classList.add('slide-transition-2');
+            slideShow.href = "https://qzmp10.github.io/etch-a-sketch/";
+            slideShow.setAttribute('style', 'background-image: linear-gradient(rgb(162, 0, 255), rgb(255, 174, 0))');
+            slideText.textContent = 'Etch-a-sketch';
+            slideShow.classList.remove('slide-transition');
+            
+            //when 2nd transition ends, remove class
+            slideShow.addEventListener('transitionend', () => {
+                slideShow.classList.remove('slide-transition-2');
+            });
+        });
+    }
+});
 
 
 
